b7b91fee095c957e6a20b638bcaf514592b321ab
fergus <fergusfettes@gmail.com>
2021-03-11 15:18:46 +0100

    updated setup and release notes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="ondewo-logging",
-    version="2.0.0",
+    version="2.0.1",
     author="Ondewo GmbH",
     author_email="info@ondewo.com",
     description="This library provides custom logging for python including error handling and timing.",
