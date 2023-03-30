131d9e3a5dc31e2d3c8ff0e69a5f5bad431966b8
Andreas Rath <andreas.rath@ondewo.com>
2023-03-30 08:53:32 +0200

    Improve setup file for better descriptions

--- setup.py
+++ setup.py
@@ -1,34 +1,46 @@
+import re
+from typing import List
+
 import setuptools
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
 
-with open('requirements.txt') as f:
-    requires = f.read().splitlines()
+def read_file(file_path: str, encoding: str = 'utf-8') -> str:
+    with open(file_path, 'r', encoding=encoding) as f:
+        return f.read()
+
+
+def read_requirements(file_path: str, encoding: str = 'utf-8') -> List[str]:
+    with open(file_path, 'r', encoding=encoding) as f:
+        requires = [
+            re.sub(r"(.*)#egg=(.*)", r"\2 @ \1", line.strip())  # replace #egg= with @
+            for line in f
+            if line.strip() and not line.startswith("#")  # ignore empty lines and comments
+        ]
+    return requires
+
+
+long_description: str = read_file('README.md')
+requires: List[str] = read_requirements('requirements.txt')
 
 setuptools.setup(
     name='ondewo-logging',
-    version='3.2.8',
+    version='3.3.0',
     author='Ondewo GmbH',
     author_email='info@ondewo.com',
     description='This library provides custom logging for python including error handling and timing.',
     long_description=long_description,
-    include_package_data=True,
     long_description_content_type='text/markdown',
     url='https://github.com/ondewo/ondewo-logging-python',
-    packages=[
-        np
-        for np in filter(
-            lambda n: n.startswith('ondewo.') or n == 'ondewo',
-            setuptools.find_packages(),
-        )
-    ],
+    packages=setuptools.find_packages(include=["ondewo*", ]),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache-2.0 license',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
     ],
-    python_requires='>2.6, !=3.0.*, !=3.1.*',
+    python_requires='>=3.6',
     install_requires=requires,
 )
