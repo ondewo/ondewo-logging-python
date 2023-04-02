d50ec717b2d6f65b2162ad07c97ae1c1d79843c9
Andreas Rath <andreas.rath@ondewo.com>
2023-04-02 19:34:27 +0200

    Next bug fix for staticmethod logger

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires: List[str] = read_requirements('requirements.txt')
 
 setuptools.setup(
     name='ondewo-logging',
-    version='3.3.1',
+    version='3.3.2',
     author='Ondewo GmbH',
     author_email='info@ondewo.com',
     description='This library provides custom logging for python including error handling and timing.',
